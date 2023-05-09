372f8fe810dcec1a29aaad6b197200ea7ee86738
D73official <usernameda73@gmail.com>
2023-05-09 14:13:28 +0700

    add more models

--- .github/This_Enviroment/Scripts/Diem_Thi/monhoc/models.py
+++ .github/This_Enviroment/Scripts/Diem_Thi/monhoc/models.py
@@ -3,3 +3,8 @@ from django.db import models
 class monhoc(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
+  dcc = models.FloatField(max_length=2)
+  dthi = models.FloatField(max_length=2)
+  dtb = models.FloatField(max_length=2)
+  t4 = models.FloatField(max_length=2)
+  result = models.CharField(max_length=255)
