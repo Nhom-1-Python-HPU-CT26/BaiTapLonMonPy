e44f936f7af6fed9c7cc68ee7042383eefb791d8
D73official <usernameda73@gmail.com>
2023-05-12 08:33:59 +0700

    fixed

--- .github/This_Enviroment/Scripts/Diem_Thi/monhoc/models.py
+++ .github/This_Enviroment/Scripts/Diem_Thi/monhoc/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 class monhoc(models.Model):
+  studentID = models.FloatField(max_length=10)
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
-  dcc = models.FloatField(max_length=2)
-  dthi = models.FloatField(max_length=2)
-  dtb = models.FloatField(max_length=2)
-  t4 = models.FloatField(max_length=2)
-  result = models.CharField(max_length=255)
+  dcc = models.IntegerField()
+  dthi = models.IntegerField()
+  dtb = models.IntegerField()
+  t4 = models.IntegerField()
+  result = models.CharField(max_length=10)
